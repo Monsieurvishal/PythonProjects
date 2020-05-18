ad1ad592f5893c06b47e8d9e82b048790ffad560
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-05-18 23:05:02 +0530

    update
    This program checks if the password is having at least one upper case at least one lower case and atleast on digit present in the program.

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -4,10 +4,24 @@
 
 import random
 import string
+    
+invalidcharacters= set(string.punctuation)
 
-size = int(input("Please enter the length for the password: "))
-password_chars = string.ascii_letters + string.digits + string.punctuation
+size = int(input("Please enter the length for the password (greater than 4:) "))
+password_chars = string.ascii_letters.upper()+string.ascii_letters.lower()+ string.digits + string.punctuation
 generated_pass = ""
-for i in range(size):
-    generated_pass += random.choice(password_chars)
-print("Random password: " + generated_pass)
+count=1
+while count==1:
+    generated_pass=''
+    for i in range(size):
+        generated_pass += random.choice(password_chars)
+        
+        
+    if (any(x.isupper() for x in generated_pass)):
+        if (any(x.islower() for x in generated_pass)):
+            if (any(x.isdigit() for x in generated_pass)):
+                if any(char in invalidcharacters for char in generated_pass):
+                    count=0
+    
+    
+print("Random password: " + generated_pass)   
